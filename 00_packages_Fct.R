356bb61e271f634dd4d0c440919f0bf8319da8d9
Marcus Ebeling <marcus.ebeling@ki.se>
2021-08-17 15:30:08 +0200

    Function and packages for the analysis

---
+++ 00_packages_Fct.R
@@ -0,0 +1,67 @@
+# Packages and functiosn for estimation
+library(tidyverse)
+library(magrittr)
+library(plotrix)
+library(mgcv)
+# 
+# packageurl <- "https://cran.r-project.org/src/contrib/Archive/MortalitySmooth/MortalitySmooth_2.3.4.tar.gz"
+# install.packages(packageurl, repos=NULL, type="source")
+# packageurl <- "https://cran.r-project.org/src/contrib/Archive/svcm/svcm_0.1.2.tar.gz"
+# install.packages(packageurl, repos=NULL, type="source")
+# 
+# library(MortalitySmooth)
+
+# Additional Function borrowed from plyr package
+
+mapvalues <- function (x, from, to, warn_missing = TRUE) 
+{
+  if (length(from) != length(to)) {
+    stop("`from` and `to` vectors are not the same length.")
+  }
+  if (!is.atomic(x)) {
+    stop("`x` must be an atomic vector.")
+  }
+  if (is.factor(x)) {
+    levels(x) <- mapvalues(levels(x), from, to, warn_missing)
+    return(x)
+  }
+  mapidx <- match(x, from)
+  mapidxNA <- is.na(mapidx)
+  from_found <- sort(unique(mapidx))
+  if (warn_missing && length(from_found) != length(from)) {
+    message("The following `from` values were not present in `x`: ", 
+            paste(from[!(1:length(from) %in% from_found)], collapse = ", "))
+  }
+  x[!mapidxNA] <- to[mapidx[!mapidxNA]]
+  x
+}
+
+# Function for partial life expectancy calculation
+
+partialLe <- function(mx, occ, sex, age, startAge){
+  ax <- rep(0.5, length(mx))
+  qx <- mx/(1+(1-ax)*mx)
+  px <- 1-qx
+  lx <- c(1000, (cumprod(px)*1000))
+  dx <- -diff(lx)
+  Lx <- lx[-1]+ax*dx
+  Tx <- rev(cumsum(rev(Lx)))
+  pE <- Tx/lx[-length(lx)]
+  out <- data.frame(age = age, occ=occ, sex=sex, startAge = startAge,pE = pE)
+  return(out)  
+}
+
+# Same function maxAge for fixed min/max calculation
+
+Lx <- function(mx, occ, sex, age, startAge){
+  ax <- rep(0.5, length(mx))
+  qx <- mx/(1+(1-ax)*mx)
+  px <- 1-qx
+  lx <- c(1000, (cumprod(px)*1000))
+  dx <- -diff(lx)
+  Lx <- lx[-1]+ax*dx
+  Tx <- rev(cumsum(rev(Lx)))
+  pE <- Lx/lx[-length(lx)]
+  out <- data.frame(age = age, occ=occ, sex=sex, startAge = startAge, Lx = Lx, lx = lx[-length(lx)])
+  return(out)  
+}
